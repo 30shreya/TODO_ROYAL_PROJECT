74c60fc4d963eb93b236806b77f8e3c7c82a1e1e
shreya bhat <shreyabhat792@gmail.com>
2022-02-21 18:38:42 +0530

    Initial project practical work.

---
+++ product/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def addProduct(request):
+    print("addProduct called....")
+    return HttpResponse("addProduct called...")
+    
+   
+  #create view  product function to display the product page
+def viewproduct(request):
+      return HttpResponse("view called....")
+
+      #create view  product function to display the product page
+
+
+ 
